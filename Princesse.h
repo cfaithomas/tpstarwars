15d2b6fe64063794e90b98def08e265d4e8c4500
eric <eric.thomas@formation-industries-fc.fr>
2020-09-21 15:57:09 +0200

    princesse.h

---
+++ Princesse.h
@@ -0,0 +1,31 @@
+//
+// Created by eric on 21/09/2020.
+//
+
+#ifndef TPSTARWARS_PRINCESSE_H
+#define TPSTARWARS_PRINCESSE_H
+
+
+#include <string>
+#include "MembreEmpire.h"
+#include "Rebel.h"
+#include "Humanoide.h"
+
+using namespace std;
+class Princesse :public Humanoide{
+
+private:
+    string couleurrobe;
+    bool etat;
+public:
+    Princesse(const string &nom, const string &couleurrobe);
+
+    void sefairekidnapper(const MembreEmpire &me);
+    void sefaireLiberer(const Rebel &re);
+    void changerRobe(const string &newcouleurrobe);
+
+
+};
+
+
+#endif //TPSTARWARS_PRINCESSE_H
